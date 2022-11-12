9581067930819aca2033a512448b7578472e38e3
Rahmawati <115470125+Rhmawthy@users.noreply.github.com>
2022-11-12 03:58:04 -0800

    Add files via upload

---
+++ days33_program list inputan.py
@@ -0,0 +1,6 @@
+#program menginput nilai atau angka
+#misal angka yang dimasukkan 9 maka outpunya list angka 1 sampai 9
+
+nilai = int(input("masukkan angka"))
+angka =[a for a in range( 1,nilai+1)]
+print(angka)
